3175bc7ef21b675783b1db8bf327a83f70dcb8f7
Sabriel Koh <sabrielkcr@gmail.com>
2026-03-06 15:56:44 +0800

    fix: sync subscribed bundle not started on user login

--- src/slic3r/GUI/MainFrame.cpp
+++ src/slic3r/GUI/MainFrame.cpp
@@ -4032,12 +4032,14 @@ void MainFrame::on_select_default_preset(SimpleEvent& evt)
         case wxID_YES: {
             wxGetApp().app_config->set_bool("sync_user_preset", true);
             wxGetApp().start_sync_user_preset(true);
+            wxGetApp().start_sync_subscribed_bundles(true);
             BOOST_LOG_TRIVIAL(info) << __FUNCTION__ << " sync_user_preset: true";
             break;
         }
         case wxID_NO:
             wxGetApp().app_config->set_bool("sync_user_preset", false);
             wxGetApp().stop_sync_user_preset();
+            wxGetApp().stop_sync_subscribed_bundles();
             BOOST_LOG_TRIVIAL(info) << __FUNCTION__ << " sync_user_preset: false";
             break;
         default:
